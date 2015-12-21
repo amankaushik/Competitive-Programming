9884eea7832a760471588797d118850b33459de3
Aman Kaushik <amankaushik104@gmail.com>
2015-12-21 18:12:21 +0530

    template.cpp file modified

--- PRIME1.cpp
+++ PRIME1.cpp
@@ -13,9 +13,6 @@
 #define lzero 						__builtin_clz // returns number of leading 0-bits of x
 #define tzero   					__builtin_ctz // returns number of trailing 0-bits of x
 #define	on_bits						__builtin_popcount //returns number of 1-bits of x
-#define s(n)                        scanf("%d",&n)
-#define p(n)						printf("%d",n)
-#define sc(n)                       scanf("%c",&n)
 #define maX(a,b)                    ( (a) > (b) ? (a) : (b))
 #define miN(a,b)                    ( (a) < (b) ? (a) : (b))
 
@@ -27,7 +24,9 @@
 #define ss(n)                       scanf("%s",n)
 #define sl(n)                       scanf("%lld",&n)
 #define sf(n)                       scanf("%lf",&n)
-#define sfi(n) 						scanf("%d", &n)
+#define s(n) 						scanf("%d", &n)
+#define p(n)						printf("%d",n)
+#define sc(n)                       scanf("%c",&n)
 
 #define l(n)						n.length()
 #define f(i, n)						for(int i = 0; i < n; i++)
@@ -35,20 +34,29 @@
 #define pb(n)						push_back(n)
 #define mp(x, y)					make_pair(x, y)
 
-typedef map<int, int>				mpi
-typedef map<char, int>				mpc
-typedef map<string, int>       		mpw
-typedef stack<int>					si
-typedef pair<int, int> 				ii
-typedef vector<ii>					vii
-typedef vector<int>					vi
-typedef long long 					ll
-typedef long int 					li
-
 using namespace std;
+
+typedef map<int, int>				mpi;
+typedef map<char, int>				mpc;
+typedef map<string, int>       		mpw;
+typedef stack<int>					si;
+typedef pair<int, int> 				ii;
+typedef vector<ii>					vii;
+typedef vector<int>					vi;
+typedef long long 					ll;
+typedef long int 					li;
 
 int main(){
 	freopen( "input.in", "r", stdin );
 	freopen( "output.out", "w", stdout );
+	int test;
+	s(test);
+	w(test) {
+		li m;
+		li n;
+		scanf("%ld%ld", &m, &n);
+		int diff = (n - m) >= 0 ? (n - m) : (- 1) * (n - m);
+		cout<<m<<" "<<n<<" "<<diff;
+	}
 	return 0;
 }
