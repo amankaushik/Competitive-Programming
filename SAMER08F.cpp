cd5cf1d5b13c8515f0daf0a4332bf7064f392c52
aman kaushik <amankaushik104@gmail.com>
2016-01-07 13:26:59 +0530

    SAMER08F.cpp added

---
+++ SAMER08F.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <map>
+
+#define gcd 						__gcd
+#define rbit						__builtin_ffs // returns 1 + index of least significant 1-bit of x
+#define lzero 						__builtin_clz // returns number of leading 0-bits of x
+#define tzero   					__builtin_ctz // returns number of trailing 0-bits of x
+#define	on_bits						__builtin_popcount //returns number of 1-bits of x
+#define maX(a,b)                    ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                    ( (a) < (b) ? (a) : (b))
+
+#define w(t)						while(t--)
+#define c(n)						cin>>n
+#define cg(n)						getline(cin, n)
+#define cco(n, m)					cout<<n<<" : "<<m<<"\n";					
+#define co(n)						cout<<n<<"\n"
+#define ss(n)                       scanf("%s",n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%lf",&n)
+#define s(n) 						scanf("%d", &n)
+#define p(n)						printf("%d",n)
+#define sc(n)                       scanf("%c",&n)
+#define emptyLine					printf("\n")
+#define l(n)						n.length()
+#define f(i, n)						for(int i = 0; i < n; i++)
+
+#define pb(n)						push_back(n)
+#define mp(x, y)					make_pair(x, y)
+
+using namespace std;
+
+typedef map<int, int>				mpi;
+typedef map<char, int>				mpc;
+typedef map<string, int>       		mpw;
+typedef stack<int>					si;
+typedef pair<int, int> 				ii;
+typedef vector<ii>					vii;
+typedef vector<int>					vi;
+typedef long long 					ll;
+typedef long int 					li;
+
+int main(){
+	freopen( "input.in", "r", stdin );
+	freopen( "output.out", "w", stdout );
+	int n;
+	while(1) {
+		s(n);
+		if(n == 0)
+			break;
+		else {
+			p(n);
+			emptyLine;
+		}
+	}
+	return 0;
+}
